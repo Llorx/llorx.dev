79f0be3f2b6c953c160a798e9936d7d9dd7d5574
Llorx <dallorx@gmail.com>
2025-11-04 09:40:42 +0100

    chg: Hobbies layout to be 4 elements

--- src/Portfolio/SectionHead/SectionHead.tsx
+++ src/Portfolio/SectionHead/SectionHead.tsx
@@ -3,7 +3,7 @@ import type { Children, Props } from "nesquick";
 import "./SectionHead.scss";
 
 export function SectionHead(props:Props<{title:Children; subtitle:Children;}>) {
-    return <div class="head">
+    return <div class="section-head">
         <div class="title">
             {props.title()}
         </div>
